4fcd12fced56db2b8703fdd65a17ec1d051a2626
Hocine <mhocine1994@gmail.com>
2022-07-03 15:09:16 +0200

    Updates

---
+++ precache-manifest.600b61326a5455650e6a8cfdc2005590.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f35191ca4d45b6ad4255",
+    "url": "/robofriends/static/css/main.b69f0e19.chunk.css"
+  },
+  {
+    "revision": "f35191ca4d45b6ad4255",
+    "url": "/robofriends/static/js/main.58ed4112.chunk.js"
+  },
+  {
+    "revision": "75dcfc39fa8879b648fd",
+    "url": "/robofriends/static/js/runtime~main.e6791cfe.js"
+  },
+  {
+    "revision": "c791082ad020cfa40ac5",
+    "url": "/robofriends/static/css/2.8dc6544c.chunk.css"
+  },
+  {
+    "revision": "c791082ad020cfa40ac5",
+    "url": "/robofriends/static/js/2.adf63c87.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/robofriends/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robofriends/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "a4ff675c39c73dc98b06cec8c22ea8b2",
+    "url": "/robofriends/index.html"
+  }
+];
